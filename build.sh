9cb6886f1c15954fc13d7c1ae69a6a8278720b18
Igor Randjelovic <rigor789@gmail.com>
2021-01-20 10:35:05 +0100

    refactor: use cd instead of pushd

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+BUILD_DIR=$(pwd)/ns-docs
 DIST_DIR=$(pwd)/dist
 
 echo "Building NativeScript docs"
 
 # Clean
 echo "Cleaning..."
-rm -rf ns-docs $DIST_DIR
+rm -rf $BUILD_DIR $DIST_DIR
 
 clone() {
   # Checkout specific branch if specified
@@ -24,61 +25,59 @@ clone() {
 
 # BUILD START #######################
 mkdir $DIST_DIR
-mkdir ns-docs; pushd ns-docs
+mkdir $BUILD_DIR
+cd $BUILD_DIR
 
-  # V6 ##############################
-  echo "Building v6.x docs"
-  mkdir v6; pushd v6
+# V6 ##############################
+echo "Building v6.x docs"
+mkdir v6; cd v6
 
-    clone "docs" "v6.x"
-    clone "nativescript-angular"
-    clone "NativeScript" "tns-core-modules"
-    clone "nativescript-sdk-examples-js"
-    clone "nativescript-sdk-examples-ng"
-    clone "nativescript-cli"
-    clone "nativescript-ui-samples"
-    clone "nativescript-ui-samples-angular"
-    clone "nativescript-ui-samples-vue"
+clone "docs" "v6.x"
+clone "nativescript-angular"
+clone "NativeScript" "tns-core-modules"
+clone "nativescript-sdk-examples-js"
+clone "nativescript-sdk-examples-ng"
+clone "nativescript-cli"
+clone "nativescript-ui-samples"
+clone "nativescript-ui-samples-angular"
+clone "nativescript-ui-samples-vue"
 
-    export Version=6.0
-    ./docs/build/site-container/build.sh
+export Version=6.0
+./docs/build/site-container/build.sh
 
-    mkdir $DIST_DIR/v6
-    cp -r ./docs/build/bin/site $DIST_DIR/v6
-    cp ./docs/build/bin/site/start/landing.html $DIST_DIR/v6/site/index.html
-    cp ./docs/version.json $DIST_DIR/v6/site
+mkdir $DIST_DIR/v6
+cp -r ./docs/build/bin/site $DIST_DIR/v6
+cp ./docs/build/bin/site/start/landing.html $DIST_DIR/v6/site/index.html
+cp ./docs/version.json $DIST_DIR/v6/site
 
-  popd
-  # END V6 ##########################
-
-
-  # V7 ##############################
-  echo "Building v7.x docs"
-  mkdir v7; pushd v7
-
-    clone "docs"
-    clone "nativescript-angular"
-    clone "NativeScript" "feat/ns7-docs" # todo: remove branch when merged
-    clone "nativescript-sdk-examples-js"
-    clone "nativescript-sdk-examples-ng"
-    clone "nativescript-cli"
-    clone "nativescript-ui-samples"
-    clone "nativescript-ui-samples-angular"
-    clone "nativescript-ui-samples-vue"
+cd $BUILD_DIR
+# END V6 ##########################
 
 
-    export Version=7.0
-    ./docs/build/site-container/build.sh
+# V7 ##############################
+echo "Building v7.x docs"
+mkdir v7; cd v7
 
-    mkdir $DIST_DIR/v7
-    cp -r ./docs/build/bin/site $DIST_DIR/v7
-    cp ./docs/build/bin/site/start/landing.html $DIST_DIR/v7/site/index.html
-    cp ./docs/version.json $DIST_DIR/v7/site
+clone "docs"
+clone "nativescript-angular"
+clone "NativeScript" "feat/ns7-docs" # todo: remove branch when merged
+clone "nativescript-sdk-examples-js"
+clone "nativescript-sdk-examples-ng"
+clone "nativescript-cli"
+clone "nativescript-ui-samples"
+clone "nativescript-ui-samples-angular"
+clone "nativescript-ui-samples-vue"
 
-  popd
-  # END V7 ##########################
 
-popd
-# BUILD END #########################
+export Version=7.0
+./docs/build/site-container/build.sh
+
+mkdir $DIST_DIR/v7
+cp -r ./docs/build/bin/site $DIST_DIR/v7
+cp ./docs/build/bin/site/start/landing.html $DIST_DIR/v7/site/index.html
+cp ./docs/version.json $DIST_DIR/v7/site
+
+cd $BUILD_DIR
+# END V7 ##########################
 
 echo "Build complete."
